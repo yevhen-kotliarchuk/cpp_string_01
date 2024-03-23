0a5fc55c05e2a890f68ddae3c6717e87efa8262f
yevhen-kotliarchuk <evgenkotliarchuk@gmail.com>
2024-03-23 21:45:01 +0100

    feat: add functions implementation

--- string_01.cc
+++ string_01.cc
@@ -1,3 +1,35 @@
 #include "string_01.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
 
-auto main() -> int { return 0; }
+auto main() -> int {
+    string example_str;
+    string example_substr;
+    cout << "Enter text for main string:";
+    cin >> example_str;
+
+    cout << "Enter text for substring:";
+    cin >> example_substr;
+
+    cout << "`string_toys::reverse` call" << endl;
+    cout << string_toys::reverse(example_str) << endl;
+
+    cout << "`string_toys::first_to_upper` call" << endl;
+    cout << string_toys::first_to_upper(example_str) << endl;
+
+    cout << "`string_toys::count_vowels` call" << endl;
+    cout << string_toys::count_vowels(example_str) << endl;
+
+    cout << "`string_toys::sum_digits` call" << endl;
+    cout << string_toys::sum_digits(example_str) << endl;
+
+    cout << "`string_toys::search_substr` call" << endl;
+    cout << string_toys::search_substr(example_str, example_substr) << endl;
+
+    cout << "`string_toys:custom_search` call" << endl;
+    cout << string_toys::custom_search(example_str.c_str(), example_substr.c_str()) << endl;
+    return 0;
+}
--- string_01.h
+++ string_01.h
@@ -1,31 +1,113 @@
 #ifndef CPP_STRING_H
 #define CPP_STRING_H
 
+#include <string>
+#include <iostream>
 
 namespace string_toys {
-    
-auto reverse(const std::string & str) -> std::string {
 
+
+auto reverse(const std::string & str) -> std::string {
+    size_t str_len = str.length();
+    std::string res_str;
+
+    for(int i = 0; i < str_len; i++) {
+        res_str += str[str_len - i - 1];
+    }
+    return res_str;
 }
 
 auto first_to_upper(const std::string & str) -> std::string {
-
+    std::string res_str;
+    for(int i = 0; i < str.length(); i++) {
+        if(i == 0 && str[i] >= 'a' && str[i] <= 'z') {
+            res_str += str[i] - 32;
+            continue;
+        }
+        if(str[i - 1] == ' ' && str[i] >= 'a' && str[i] <= 'z') {
+            res_str += str[i] - 32;
+            continue;
+        }
+        res_str += str[i];
+    }
+    return res_str;
 }
 
 auto count_vowels(const std::string & str) -> int {
-    
+    char vowels_lower[] = {'a', 'e', 'i', 'o', 'u'};
+    int counter = 0;
+    for(char str_el : str) {
+        for(char vowel_char : vowels_lower) {
+            if(str_el == vowel_char || str_el == (vowel_char - 32)) {
+                counter++;
+            }
+        }
+    }
+    return counter;
 }
 
 auto sum_digits(const std::string & str) -> int {
-    
+    int sum = 0;
+    std::string tmp_num_str;
+    for(char el: str) {
+        if(el >= '0' && el <= '9') {
+            tmp_num_str += el;
+        } else if (!tmp_num_str.empty()) {
+            sum += stoi(tmp_num_str);
+            tmp_num_str.clear();
+        }
+    }
+    return sum;
 }
 
 auto search_substr(const std::string & str, const std::string & substr) -> int {
-
+    int position = -1;
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] == substr[0]) {
+            position = i;
+            for(int j = 1; j < substr.length(); j++) {
+                if(str.length() <= j || str[i + j] != substr[j]) {
+                    position = -1;
+                    break;
+                }
+            }
+            if(position != -1) {
+                break;
+            }
+        }
+    }
+    return position;
 }
 
-auto custom_serach(const char * str, const char * substr) -> int {
-    
+auto custom_search(const char * str, const char * substr) -> int {
+    int position = -1;
+    int ref_index = 0;
+    while(*str != '\0') {
+        if(*str == *substr) {
+            position = ref_index;
+            str++;
+            substr++;
+            ref_index++;
+
+            while(*substr != '\0' && *str != '\0') {
+                if(*str == *substr) {
+                    str++;
+                    substr++;
+                    ref_index++;
+                    continue;
+                } else {
+                    position = -1;
+                    break;
+                }
+            }
+            if(position != -1) {
+                break;
+            }
+        }
+        str++;
+        ref_index++;
+    }
+    return position;
 }
 
 }
